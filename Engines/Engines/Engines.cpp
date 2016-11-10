057cf17db860a3ec1b75244db2f53e2bd0e98735
Jiorgon <navarrotrocal@hotmail.com>
2016-11-10 17:44:46 +0100

    Cambio color y tiempo

--- Engines/Engines/Engines.cpp
+++ Engines/Engines/Engines.cpp
@@ -36,13 +36,13 @@ int main(int argc, char* args[])
 			screenSurface = SDL_GetWindowSurface(window);
 
 			//Fill the surface white
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0x00, 0xFF));
 
 			//Update the surface
 			SDL_UpdateWindowSurface(window);
 
 			//Wait two seconds
-			SDL_Delay(2000);
+			SDL_Delay(10000);
 		}
 	}
 	//Destroy window
