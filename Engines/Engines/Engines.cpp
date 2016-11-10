f078fed3851ba08846199b34fd1e67b3a4b5cde8
Bosk723 <bosk723@hotmail.com>
2016-11-10 16:23:23 +0100

    Window

--- Engines/Engines/Engines.cpp
+++ Engines/Engines/Engines.cpp
@@ -3,13 +3,52 @@
 
 #include "stdafx.h"
 #include <SDL.h>
+#include <stdio.h>
 
-int main(int argc, char ** argv)
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char* args[])
 {
-	SDL_Init(SDL_INIT_VIDEO);
+	//The window we'll be rendering to
+	SDL_Window* window = NULL;;
 
-	// game code eventually goes here
+	//The surface contained by the window
+	SDL_Surface* screenSurface = NULL;
 
+	//Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+	}
+	else
+	{
+		//Create window
+		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (window == NULL)
+		{
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+		}
+		else
+		{
+			//Get window surface
+			screenSurface = SDL_GetWindowSurface(window);
+
+			//Fill the surface white
+			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+
+			//Update the surface
+			SDL_UpdateWindowSurface(window);
+
+			//Wait two seconds
+			SDL_Delay(2000);
+		}
+	}
+	//Destroy window
+	SDL_DestroyWindow(window);
+
+	//Quit SDL subsystems
 	SDL_Quit();
 
 	return 0;
